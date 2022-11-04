b64e2d67d36474ba668f0eb2153fb3888921f141
Robbie Prokop <robbieprokop@gmail.com>
2022-11-04 20:12:56 +0400

    create showError func

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // main JS file
 //If statements for everything
 const form = document.getElementById("form");
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
-const password2 = document.getElementById("password2").value;
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
 //showError function
-const showError = (input, message) => {};
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+};
 const showSuccess = (input) => {};
 
 //Add Event Listener to for msubmit
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (!username) {
-    return showError(
-      username,
-      "Get your poop in a group and fill in the Username"
-    );
+  if (!username.value) {
+    showError(username, "Please fill in the Username");
   }
   showSuccess(username);
 });
