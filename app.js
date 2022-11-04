9d262bed172d4193a12a04392aaff73a9d7672c7
Robbie Prokop <robbieprokop@gmail.com>
2022-11-04 16:56:08 +0400

    begin JS work

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // main JS file
+//If statements for everything
+const form = document.getElementById("form");
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+const password2 = document.getElementById("password2").value;
+
+//showError function
+const showError = (input, message) => {};
+const showSuccess = (input) => {};
+
+//Add Event Listener to for msubmit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!username) {
+    return showError(
+      username,
+      "Get your poop in a group and fill in the Username"
+    );
+  }
+  showSuccess(username);
+});
