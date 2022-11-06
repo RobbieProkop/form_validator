d94c4924a133300feccc19967e0da472b7e86da1
Robbie Prokop <robbieprokop@gmail.com>
2022-11-06 15:23:50 +0400

    complete simple if validation

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ const showSuccess = (input) => {
   formControl.className = "form-control success";
 };
 
+// return String(email)
+//     .toLowerCase()
+//     .match(
+//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     );
+
+// CHeck for valid email
+const isValidEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 //Add Event Listener to for msubmit
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -30,6 +45,8 @@ form.addEventListener("submit", (e) => {
 
   if (!email.value) {
     showError(email, "Please enter an email");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Please enter a valid email");
   } else {
     showSuccess(email);
   }
