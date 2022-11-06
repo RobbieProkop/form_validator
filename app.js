c9c3edf11026336ba8994f5c81239d800c0cf931
Robbie Prokop <robbieprokop@gmail.com>
2022-11-06 16:06:24 +0400

    check min and max length

--- app.js
+++ app.js
@@ -18,19 +18,34 @@ const showSuccess = (input) => {
   formControl.className = "form-control success";
 };
 
+//get field name for error message
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
 //check to make sure required input is present
 const checkRequired = (inputArr) => {
-  console.log("inputArr :>> ", inputArr);
   inputArr.forEach((input) => {
     if (!input.value.trim()) {
       return showError(input, `${getFieldName(input)} is required`);
     }
-    if (input.id === "email") {
-      console.log("input.value :>> ", input.value);
-      isValidEmail(input.value);
-    }
+    // if (input.id === "email") {
+    //   console.log("obj :>> ", isValidEmail(input));
+    //   isValidEmail(input.value);
+    // }
     showSuccess(input);
   });
+};
+
+// check minimum length of username and password
+const checkLength = (input, min, max) => {
+  if (input.value.length < min || input.value.length > max) {
+    return showError(
+      input,
+      `${getFieldName(input)} must be between ${min}-${max} characters`
+    );
+  }
+  showSuccess(input);
 };
 
 // CHeck for valid email
@@ -42,15 +57,13 @@ const isValidEmail = (email) => {
     );
 };
 
-const getFieldName = (input) => {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-};
-
 //Add Event Listener to for msubmit
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
 
   // Simple conditional validation. Not good clean practice
 
